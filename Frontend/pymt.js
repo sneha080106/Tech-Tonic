abea013b8fef37247d4b0affe516852a11b6590e
sneha080106 <snehasri263@gmail.com>
2026-09-06 08:29:25 +0530

    updated

--- Frontend/pymt.js
+++ Frontend/pymt.js
@@ -47,7 +47,7 @@ proceedBtn.addEventListener("click", async function () {
     };
 
     try {
-        const response = await fetch("/api/orders", ... {
+        const response = await fetch("/api/orders", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
