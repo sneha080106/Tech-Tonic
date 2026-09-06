4ba3bf751600de65f90edc43484f4cf5853d5c10
sneha080106 <snehasri263@gmail.com>
2026-09-06 08:41:37 +0530

    Update mailer.js

--- Backend/utils/mailer.js
+++ Backend/utils/mailer.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
-  secure: true, // true for 465, false for other ports
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
